eff280a90c24e23a94ddbdbf829ef60baac378e9
Alberto <albertpk97@gmail.com>
2019-02-25 12:53:58 +0100

    commit Con duda para el script #2

--- R/read_lol_kaggle_dataset.R
+++ R/read_lol_kaggle_dataset.R
@@ -6,6 +6,19 @@
 #' @export
 #'
 #' @examples
-read_lol_kaggle_dataset = function(input_df) {
-  print("prueba")
+read_lol_kaggle_dataset <- function(input_df) {
+  Data <- read.csv(file = input_df, header = TRUE, sep = ",")
+
+  for (row in 1:nrow(Data)) {
+    matchId <- Data[row, "Address"]
+    #Ahora habria que recorrer las columnas de equipo por evento, y añadir a nuestro dataframe una fila por cada elemento
+    #de dicho recorrido, con la division de columnas adecuadas, pero no estoy seguro del acceso
+  }
+
+  return(Data)
 }
+
+#print(read_lol_kaggle_dataset("inst/extdata/LeagueofLegends.csv")$bTowers[1])
+print(nrow(read_lol_kaggle_dataset("inst/extdata/LeagueofLegends.csv")))
+
+
