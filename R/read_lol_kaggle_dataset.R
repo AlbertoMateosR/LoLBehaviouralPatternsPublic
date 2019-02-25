bb9fcdc8d6343aea84b0d03ea21b337223a6dcaa
victor <victor.rodriguez@inv.uam.es>
2019-02-25 17:01:43 +0100

    Mejoras en script de preprocesado

--- R/read_lol_kaggle_dataset.R
+++ R/read_lol_kaggle_dataset.R
@@ -1,6 +1,6 @@
 #' Title
 #'
-#' @param input_df
+#' @param input_df String containing the filepath of the input file
 #'
 #' @return
 #' @export
@@ -14,6 +14,14 @@ read_lol_kaggle_dataset <- function(input_df) {
     #Ahora habria que recorrer las columnas de equipo por evento, y añadir a nuestro dataframe una fila por cada elemento
     #de dicho recorrido, con la division de columnas adecuadas, pero no estoy seguro del acceso
   }
+
+  bData <- Data %>%
+    mutate(bTowers = purrr::map(bTowers, function(x) {
+      jsonlite::fromJSON(stringr::str_replace_all(x, "'", '"')) %>% as.data.frame()
+    })) %>%
+    tidyr::unnest(bTowers) %>%
+    dplyr::select(Address, Timestamp = V1 , Lane = V2, Tower_Type = V3) %>%
+    tidyr::unite("Event", sep = "/", Lane, Tower_Type)
 
   return(Data)
 }
