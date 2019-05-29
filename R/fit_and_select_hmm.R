d59c414de5b7e3f93c7bf56a004bd3a3315d3402
victor <victor.rodriguez@inv.uam.es>
2019-05-29 17:48:09 +0200

    Esqueleto de funciones fit_and_select_hmm, fit_hmms_lol_processed_data

---
+++ R/fit_and_select_hmm.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param sts_data
+#'
+#' @return Return a list with both the best model (field $best) and the list of a
+#' all fitted models (field $all)
+#' @export
+#'
+#' @examples
+fit_and_select_hmm = function(sts_data) {
+
+}
---
+++ R/fit_hmms_lol_processed_data.R
@@ -0,0 +1,10 @@
+#' Title
+#'
+#' @param processed_data Estructura conocida de campos
+#'
+#' @return Misma estructura que argumento original, pero con modelos en vez de con datos
+#' @export
+#'
+#' @examples
+fit_hmms_to_lol_processed_data = function(processed_data) {
+}
