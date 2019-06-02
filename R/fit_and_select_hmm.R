25ff8bb879963726d4c4ca4ff5585d90b02e6b1f
AlbertoMateosR <albertpk97@gmail.com>
2019-06-02 13:45:01 +0200

    #8 ¿Añado mas hilos?

--- R/fit_and_select_hmm.R
+++ R/fit_and_select_hmm.R
@@ -8,5 +8,24 @@
 #'
 #' @examples
 fit_and_select_hmm = function(sts_data) {
+  fittedModels <- list()
 
+  hmmMod <- build_hmm(sts_data, n_states = 2)
+  hmmFittedMod <- fit_model(hmmMod, threads = 3)
+
+  fittedModels <- fittedModels %>% append(hmmFittedMod)
+  bestModel <- hmmFittedMod$model
+
+  for (i in 3:8) {
+    hmmMod = build_hmm(sts_data, n_states = i)
+    hmmFittedMod = fit_model(hmmMod, threads = 3)
+
+    fittedModels <- fittedModels %>% append(hmmFittedMod)
+
+    if (BIC(bestModel) > BIC(hmmFittedMod$model)){
+      bestModel <- hmmFittedMod$model
+    }
+  }
+
+  return(list(best = bestModel, all = fittedModels))
 }
